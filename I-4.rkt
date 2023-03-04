5df6baa839d9732acb0638ada7b57bf1954ddbf8
Rubén M <dev.mantecon@gmail.com>
2023-03-04 15:35:41 +0100

    save

---
+++ I-4.rkt
@@ -0,0 +1,84 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname I-4) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+(define HEIGHT 300) ; distances in pixels 
+(define WIDTH  100)
+(define YDELTA 3)
+ 
+(define BACKG  (empty-scene WIDTH HEIGHT))
+(define ROCKET (rectangle 5 30 "solid" "red"))
+ 
+(define CENTER (/ (image-height ROCKET) 2))
+
+
+; An LRCD (for launching rocket countdown) is one of:
+; – "resting"
+; – a Number between -3 and -1
+; – a NonnegativeNumber 
+; interpretation a grounded rocket, in countdown mode,
+; a number denotes the number of pixels between the
+; top of the canvas and the rocket (its height)
+
+; LRCD -> Image
+; renders the state as a resting or flying rocket 
+(define (show x)
+  (cond
+    [(string? x) (place-image ROCKET 10 (- HEIGHT CENTER) BACKG)]
+    [(<= -3 x -1) (place-image (text (number->string x) 20 "red")
+                                10 (* 3/4 WIDTH)
+                                (place-image ROCKET 10 (- HEIGHT CENTER) BACKG))]
+    [(>= x 0) (place-image ROCKET 10 (- x CENTER) BACKG)]
+    ))
+
+(define (show2 x)
+  (place-image (cond
+                 [(string? x)(ROCKET 10 (- HEIGHT CENTER) BACKG)]
+                 [(<= -3 x -1) (text (number->string x) 20 "red")
+                               10 (* 3/4 WIDTH)
+                               (place-image ROCKET 10 (- HEIGHT CENTER) BACKG)]
+                 [>= x 0] ROCKET 10 (-x CENTER) BACKG)))
+
+(check-expect
+ (show "resting")
+ (place-image ROCKET 10 (- HEIGHT CENTER) BACKG))
+ 
+(check-expect
+ (show -2)
+ (place-image (text "-2" 20 "red")
+              10 (* 3/4 WIDTH)
+              (place-image ROCKET 10 (- HEIGHT CENTER) BACKG)))
+ 
+(check-expect
+ (show 53)
+ (place-image ROCKET 10 (- 53 CENTER) BACKG))
+
+(check-expect
+ (show HEIGHT)
+ (place-image ROCKET 10 (- HEIGHT CENTER) BACKG))
+
+(check-expect
+ (show 0)
+ (place-image ROCKET 10 (- 0 CENTER) BACKG))
+ 
+; LRCD KeyEvent -> LRCD
+; starts the countdown when space bar is pressed, 
+; if the rocket is still resting 
+(define (launch x ke)
+  (cond
+    [(string? x)]
+    [(<= -3 x -1) x]
+    [(>= x 0) ] x))
+
+(check-expect (launch "resting" " ") -3)
+(check-expect (launch "resting" "a") "resting")
+(check-expect (launch -3 " ") -3)
+(check-expect (launch -1 " ") -1)
+(check-expect (launch 33 " ") 33)
+(check-expect (launch 33 "a") 33)
+ 
+; LRCD -> LRCD
+; raises the rocket by YDELTA,
+;  if it is moving already 
+(define (fly x)
+  x)
+
