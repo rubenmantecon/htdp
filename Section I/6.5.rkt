a6706e4958c93813dc011bd93b4012766668b790
Rubén M <dev.mantecon@gmail.com>
2023-05-29 11:49:03 +0200

    Finished section 1

---
+++ Section I/6.5.rkt
@@ -0,0 +1,35 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |6|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; Any -> Boolean
+; is the given value an element of TrafficLight
+(define (light? x)
+  (cond
+    [(string? x) (or (string=? "red" x)
+                     (string=? "green" x)
+                     (string=? "yellow" x))]
+    [else #false]))
+
+(define MESSAGE
+  "traffic light expected, given some other value")
+ 
+; Any Any -> Boolean
+; are the two values elements of TrafficLight and, 
+; if so, are they equal
+ 
+(check-expect (light=? "red" "red") #true)
+(check-expect (light=? "red" "green") #false)
+(check-expect (light=? "green" "green") #true)
+(check-expect (light=? "yellow" "yellow") #true)
+ 
+(define (light=? a-value another-value)
+  (if (and (light? a-value) (light? another-value))
+      (string=? a-value another-value)
+      (cond
+        [(light? a-value) (error "The second value is not a traffic")]
+        [(light? another-value) (error "The first value is not a traffic light")]
+        [else (error MESSAGE)])))
+
+(check-error (light=? "red" "something else") "The second value is not a traffic")
+(check-error (light=? "something else" "red") "The first value is not a traffic light")
+(check-error (light=? "aha" "huh") MESSAGE)
