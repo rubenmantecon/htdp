0d98ace86923e3b244c0de66769ad76e9acad513
Rubén Mantecón <dev.mantecon@gmail.com>
2023-08-22 13:36:24 +0200

    save && git push

--- Section II/10.4-a-graphical-editor-revisited.rkt
+++ Section II/10.4-a-graphical-editor-revisited.rkt
@@ -2,3 +2,20 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname 10.4-a-graphical-editor-revisited) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
 ; 10.4 A Graphical Editor, Revisited
+
+(define-struct editor [pre post])
+; An Editor is a structure:
+;   (make-editor Lo1S Lo1S)
+
+; An Lo1S is one of: 
+; – '()
+; – (cons 1String Lo1S)
+
+; Lo1s -> Lo1s 
+; produces a reverse version of the given list 
+ 
+(check-expect
+  (rev (cons "a" (cons "b" (cons "c" '()))))
+  (cons "c" (cons "b" (cons "a" '()))))
+ 
+(define (rev l) l)
