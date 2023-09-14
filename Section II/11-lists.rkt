061031ed3832bbac792b4445f3fb637b9ee54982
Ruben M <dev.mantecon@gmail.com>
2023-09-14 13:27:30 +0200

    save

--- Section II/11-lists.rkt
+++ Section II/11-lists.rkt
@@ -2,4 +2,36 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname 11-lists) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
 ; 11 Design by Composition
-; 11.1 The list Function
+; 11.3 Auxiliary Functions that Recur
+
+; List-of-numbers -> List-of-numbers 
+; rearranges alon in descending order
+(define (sort> alon)
+  (cond
+    [(empty? alon) '()]
+    [else
+     (insert (first alon) (sort> (rest alon)))]))
+
+(check-expect (sort> '()) '())
+(check-expect (sort> (list 3 2 1)) (list 3 2 1))
+(check-expect (sort> (list 1 2 3)) (list 3 2 1))
+(check-expect (sort> (list 12 20 -5))
+              (list 20 12 -5))
+
+
+; Number List-of-numbers -> List-of-numbers
+; inserts n into the sorted list of numbers alon
+(define (insert n alon)
+  (cond
+    [(empty? alon) (list n)]
+    [else (if (>= n (first alon))
+              (cons n alon)
+              (cons (first alon) (insert n (rest alon))))]
+    ))
+(check-expect (insert 4 '()) (list 4))
+(check-expect (insert 5 (list 6)) (list 6 5))
+(check-expect (insert 5 (list 4)) (list 5 4))
+(check-expect (insert 12 (list 20 -5))
+              (list 20 12 -5))
+
+; Recursive insertion sort, cool
