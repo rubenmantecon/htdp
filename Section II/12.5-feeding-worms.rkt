da9ed7512e42ab730d495be009633c30003a6599
Ruben M <dev.mantecon@gmail.com>
2023-11-06 12:02:48 +0100

    Worm/Snake!

---
+++ Section II/12.5-feeding-worms.rkt
@@ -0,0 +1,3 @@
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname 12.4-word-games-the-heart-of-the-problem) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+
+;; 12.5 Feeding Worms
