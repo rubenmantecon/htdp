4c3a3524e43c573c020b22567d92a870e8ef1063
Rubén M. <dev.mantecon@gmail.com>
2024-01-05 10:33:02 +0100

    save

--- Section II/12.4-word-games-the-heart-of-the-problem.rkt
+++ Section II/12.4-word-games-the-heart-of-the-problem.rkt
@@ -10,10 +10,18 @@
 ; produces a list of all the possible arrangements a word can have
 (define (arrangements w)
   (cond
-    [(empty? w) '()]
-    [else (insert-everywhere/in-all-words (first w)
-                                          (arrangements (rest w)))]))
+    [(empty? w) (list '())]
+    [else (insert-everywhere/in-all-words (first w) (arrangements (rest w)))]))
 
+(check-expect (arrangements (list "e" "r")) (list (list "e" "r") (list "r" "e")))
+
+(check-expect (arrangements (list "d" "e" "r"))
+              (list (list "d" "e" "r")
+                    (list "e" "d" "r")
+                    (list "e" "r" "d")
+                    (list "r" "e" "d")
+                    (list "r" "d" "e")
+                    (list "d" "r" "e")))
 
 ;; Ex 212
 
@@ -32,56 +40,88 @@
 (define (insert-everywhere/in-all-words s low)
   (cond
     [(empty? low) '()]
-    [else (append
-           (insert-everywhere/word s (first low)) (insert-everywhere/in-all-words s (rest low)))]))
+    [else (list (... s (first low)) (insert-everywhere/in-all-words s (rest low)))]))
 
-(check-expect (insert-everywhere/in-all-words "x" '()) '())
-(check-expect (insert-everywhere/in-all-words "x" (list
-                                                   (list "a")))
-              (list
-               (list "x" "a")
-               (list "a" "x")))
+(check-expect (insert-everywhere/in-all-words "e" (list (list "r")))
+              (list (list "e" "r") (list "r" "e")))
 
-(check-expect (insert-everywhere/in-all-words "x" (list
-                                                   (list "a" "b")
-                                                   (list "c" "d")))
-              (list
-               (list "x" "a" "b")
-               (list "a" "x" "b")
-               (list "a" "b" "x")
-               (list "x" "c" "d")
-               (list "c" "x" "d")
-               (list "c" "d" "x")))
+(check-expect (insert-everywhere/in-all-words "d" (list (list "e" "r")))
+              (list (list "d" "e" "r")
+                    (list "e" "d" "r")
+                    (list "e" "r" "d")
+                    (list "r" "e" "d")
+                    (list "r" "d" "e")
+                    (list "d" "r" "e")))
+
+; 1String Word -> List-of-words
+; produces all the permutations with the 1String for a Word
+(define (insert-everywhere/words s w)
+  (cond
+    [(empty? w) '()]
+    [(empty? (rest w)) (insert-end/word s w)]
+    [else (list (insert-beginning/word s w) ...)]))
+
+(check-expect (insert-everywhere/words "x" '()) '())
+
+(check-expect (insert-everywhere/words "e" (list "r")) (list (list "e" "r") (list "r" "e")))
+
+(check-expect (insert-everywhere/words "d" (list "e" "r"))
+              (list (list "d" "e" "r")
+                    (list "e" "d" "r")
+                    (list "e" "r" "d")
+                    (list "r" "e" "d")
+                    (list "r" "d" "e")
+                    (list "d" "r" "e")))
+
+; 1String Word -> List-of-words
+; produce permutations with the 1String in between
+(define (insert-middle/words s w)
+  (cond
+    [(empty? w) '()]
+    [(empty? (rest w)) w]
+    [else (... (first w) (list s) (rest w))]))
+
+(check-expect (insert-middle/words "x" (list "a" "b" "c"))
+              (list (list "a" "x" "b" "c") (list "a" "b" "x" "c")))
 
 ; 1String Word -> Word
 ; inserts the 1String in all possible positions of a Word
 (define (insert-everywhere/word s w)
   (cond
-    [(empty? w) '()]
-    [(empty? (rest w)) (list
-                       (insert-first/word s w)
-                       (insert-last/word s w))]
-    [else (list
-           (insert-first/word s w)
-           (insert-in-between/word s w)
-           (insert-last/word s w))])) 
+    [(empty? w) (list s)]
+    [else (cons s (cons (first w) (insert-everywhere/word s (rest w))))]))
 
-(define (insert-first/word s w)
+(check-expect (insert-everywhere/word "x" (list "a")) (list "x" "a" "x"))
+(check-expect (insert-everywhere/word "x" (list "a" "b")) (list "x" "a" "x" "b" "x"))
+
+; 1String Word -> Word
+; inserts the 1String at the beginning of the Word
+(define (insert-beginning/word s w)
   (cond
     [(empty? w) '()]
     [else (cons s w)]))
 
-(define (insert-last/word s w)
+(check-expect (insert-beginning/word "x" '()) '())
+(check-expect (insert-beginning/word "x" (list "a")) (list "x" "a"))
+
+; 1String Word -> Word
+; inserts the 1String at the end of the Word
+(define (insert-end/word s w)
   (cond
     [(empty? w) '()]
     [else (append w (list s))]))
 
-(define (insert-in-between/word s w)
+(check-expect (insert-end/word "x" '()) '())
+(check-expect (insert-end/word "x" (list "a")) (list "a" "x"))
+
+; 1String Word -> Word
+; inserts the 1String in between all 1Strings of a Word
+(define (insert-middle/word s w)
   (cond
     [(empty? w) '()]
     [(empty? (rest w)) w]
-    [else (append (list (first w)) (list s) (insert-in-between/word s (rest w)))]))
+    [else (append (list (first w)) (list s) (insert-middle/word s (rest w)))]))
 
-
-(check-expect (insert-in-between/word "x" (list "a" "b" "c"))
-              (list "a" "x" "b" "x" "c"))
+(check-expect (insert-middle/word "x" (list "a")) (list "a"))
+(check-expect (insert-middle/word "x" (list "a" "b" "c")) (list "a" "x" "b" "x" "c"))
+(check-expect (insert-middle/word "x" (list "a" "b")) (list "a" "x" "b"))
