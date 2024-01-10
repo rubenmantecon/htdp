6f1d2805eb6b0ae0deccd6b00253a00a387c4a74
MANTECON FERRE <manteconfr@diba.cat>
2024-01-10 16:29:07 +0100

    save

--- Section II/12.5-feeding-worms.rkt
+++ Section II/12.5-feeding-worms.rkt
@@ -4,44 +4,70 @@
 ;; Ex 215
 
 ;; Constants
+(define WIDTH 280)
+(define HEIGHT 80)
+(define WW-X 15)
+(define WW-Y 30)
+(define WW-DIR "right")
 (define DIAMETER 2)
 (define PIECE (circle DIAMETER "solid" "red")) 
-(define BACKGROUND (empty-scene 120 80))
+(define BACKGROUND (empty-scene WIDTH HEIGHT))
+(define RATE 2)
 
 ;; Data definitions
+;; A Worm is a Posn
 
-; A WorldState is a Posn
-; interpretation the location of the worm at each moment in time
+;; A Direction is one of:
+;; - "up"
+;; - "down"
+;; - "left"
+;; - "right"
+
+;; A Worm World (ww) is a structure
+(define-struct ww [worm direction])
+;; (make-ww Posn String)
+;; interpretation: the position of the worm and its current direction
+
+(define WW1
+  (make-ww
+   (make-posn WW-X WW-Y)
+   WW-DIR))
 
 ;; World functions
-
 ; WorldState -> Image 
 ; renders the worm based on the current WorldState (Posn)
-(define (render cw) (place-image PIECE (posn-x cw) (posn-y cw) BACKGROUND))
+(define (render cw) (place-image PIECE (posn-x (ww-worm cw)) (posn-y (ww-worm cw)) BACKGROUND))
 
 ; WorldState -> WorldState  
 ; updates the state of the world after each CPU clock tick
 (define (tock cw)
- (make-posn (+ (posn-x cw) DIAMETER) (+ (posn-y cw) DIAMETER)))
- 
+  (cond
+    [(key=? (ww-direction cw) "up") (make-ww (make-posn (posn-x (ww-worm cw)) (sub1 (posn-y (ww-worm cw)))) "up")] 
+    [(key=? (ww-direction cw) "down") (make-ww (make-posn (posn-x (ww-worm cw)) (add1 (posn-y (ww-worm cw)))) "down")]
+    [(key=? (ww-direction cw) "right") (make-ww (make-posn (add1 (posn-x (ww-worm cw))) (posn-y (ww-worm cw))) "right")]
+    [(key=? (ww-direction cw) "left") (make-ww (make-posn (sub1 (posn-x (ww-worm cw))) (posn-y (ww-worm cw))) "left")]
+    [else cw]))
+
 
 ; KeyEvent Worm -> Worm
 ; updates the state of the worm upon a KeyEvent
 (define (ke-h cw ke)
- (cond
-  [(key=? ke "up") (make-posn (posn-x cw)  (- (posn-y cw) DIAMETER))]
-  [(key=?  ke "down") (make-posn (posn-x cw) (+ (posn-y cw) DIAMETER))]
-  [(key=? ke "left") (make-posn (- (posn-x cw) DIAMETER) (posn-y cw))]
-  [(key=? ke "right" ) (make-posn (+ (posn-x cw) DIAMETER) (posn-y cw))]
-  [else cw]))
+  (cond
+    [(key=? "up" ke) (make-ww (make-posn (posn-x (ww-worm cw)) (sub1 (posn-y (ww-worm cw)))) ke)]
+    [(key=? "down" ke) (make-ww (make-posn (posn-x (ww-worm cw)) (add1 (posn-y (ww-worm cw)))) ke)]
+    [(key=? "right" ke) (make-ww (make-posn (add1 (posn-x (ww-worm cw))) (posn-y (ww-worm cw))) ke)]
+    [(key=? "left" ke) (make-ww (make-posn (sub1 (posn-x (ww-worm cw))) (posn-y (ww-worm cw))) ke)]
+    [else cw]
+    
+    ))
   
 
 ;; WorldState -> Boolean
 ;; evaluates, after each event, if the conditions to stop the program are satisifed
 ;(define (end? world) '())
 
-(define (main world)
- (big-bang world
+(define (main-worm cw)
+ (big-bang cw
   [on-key ke-h]
   [on-tick tock 1]
   [on-draw render]))
