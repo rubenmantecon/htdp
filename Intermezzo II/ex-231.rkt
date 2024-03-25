1352f28abb6f1fe21147fe0fe1011763167f0030
Rubén M. <dev.mantecon@gmail.com>
2024-03-25 21:15:05 +0100

    finish ex-231

---
+++ Intermezzo II/ex-231.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname ex-231) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(list 1 "a" 2 #false 3 "c")
+
+(list)
+
+(list (list "alan" 1000)
+      (list "barb" 2000)
+      (list "carl" 1500))
+
+(cons 1 (cons "a" (cons 2 (cons #false (cons 3 (cons "c" '()))))))
+
+(cons '() '())
+
+(cons
+ (cons "alan" (cons 1000 '()))
+ (cons
+  (cons "barb" (cons 2000 '()))
+  (cons
+   (cons "carl" (cons 1500 '())) '())))
