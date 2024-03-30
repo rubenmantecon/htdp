f917f6406cf16adb0ea8ce935d48172ef168a923
Rubén M. <dev.mantecon@gmail.com>
2024-03-30 11:35:28 +0100

    Start Section III, upgrade to ISL, finish ex-235!

---
+++ Section III/ex-235.rkt
@@ -0,0 +1,14 @@
+; List-of-string -> Boolean
+; does l contain "atom"
+(define (contains-atom? l)
+  (contains? "l" atom))
+
+; List-of-string -> Boolean
+; does l contain "basic"
+(define (contains-basic? l)
+  (contains? "basic" l))
+
+; List-of-string -> Boolean
+; does l contain "zoo"
+(define (contains-zoo? l)
+  (contains? "zoo" l))
