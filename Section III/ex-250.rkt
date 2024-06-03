807da91bc03c51943a73da0dd8f7754f6659b1d3
Rubén M. <dev.mantecon@gmail.com>
2024-06-03 10:30:34 +0200

    finish ex-250

---
+++ Section III/ex-250.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex-250) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp")) #f)))
+; Number [Number -> Number] -> [List-of Number]
+; tabulates a trigonometric function
+; between n and 0 (inc. in a list)
+(define (tabulate fn n)
+  (cond
+    [(= n 0) (list (fn 0))]
+    [else (cons
+           (fn n)
+           (tabulate fn (sub1 n)))]))
