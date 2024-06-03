f929b9372f223e05a99cd807dd46ec2832bb26fa
Rubén M. <dev.mantecon@gmail.com>
2024-06-03 10:30:22 +0200

    finish ex-244

---
+++ Section III/ex-244.rkt
@@ -0,0 +1,42 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex-244) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp")) #f)))
+; (define (name variable variable ...) expr)
+; (f x) fits (name variable variable ...)
+; (x 10) must be an expr
+; an expr can be (name expr expr ...)
+; x is a name (of a function value to be passed in)
+; 10 is a a number
+
+; The signature of this function would be
+; Function -> Number
+; (define (f x) (x 10))
+; where Function is a function with one Number input
+
+; (define (f x) (x f))
+
+; (define (name variable variable ...) expr)
+; (f x) fits
+; (x f) fits expr = (name expr expr ...)
+
+; This would be a function that consumes a function
+; and returns a function
+
+; Function -> Function
+; (define (f x) (x f))
+; where Function is a function that takes a function and outputs a function
+
+(define (f x y) (x 'a y 'b))
+; (define (name variable variable ...) expr)
+; fits (f x y)
+
+
+; This could be a function that consumes 2 functions
+; and returns a list
+
+; FunctionA FunctionB -> Function
+; (define (f x y) (x 'a f 'b))
+
+;(define (f x y) (x 'a y 'b))
+;(define (fun1 x y z) (y '(x z)))
+;(define (fun2 x) x)
