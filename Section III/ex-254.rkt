6886541d188258ff95dc446915184e8f120db2c6
MANTECON FERRE <manteconfr@diba.cat>
2024-06-05 15:27:26 +0200

    finish ex-254

---
+++ Section III/ex-254.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex-254) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; [ListOf Number] (Number Number -> Boolean) -> [ListOf Number]
+(define (sort-n lon fn) '(...))
+
+; [ListOf String] (String String -> Boolean) -> [ListOf String]
+(define (sort-s los fn) '(...))
+
+; [ListOf X] (X X -> Boolean) -> [ListOf X]
+(define (sort-abs l f) '(...))
+
+
+; [List-of Number] (Number Number -> Boolean) -> [List-of Number]
+;         |           |      |          |                |
+; [List-of String] (String String -> Boolean) -> [List-of String]
