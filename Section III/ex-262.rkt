d66de4ccac0ef118decd065b8a0981efec2db334
Rubén M. <dev.mantecon@gmail.com>
2024-06-12 15:09:33 +0200

    finish ex-262. Unsure about why make-identity-matrix works
    I believe it's an elegant solution, and expresses exactly what I had in
    mind. On the other hand, I've been going a bit through trial and error
    on the last part, more than being systematic.
    
    Still, I believe I've been plenty systematic, and can be satisfied with
    the the simplicity of the program.

--- Section III/ex-262.rkt
+++ Section III/ex-262.rkt
@@ -1,15 +1,24 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex-262) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; [X] [List-of X] X Number -> [List-of X]
+; insert item at index
+(define (insert l x i)
+  (cond
+    [(empty? l) (cons x '())]
+    [(= i 0) (cons x l)]
+    [else (cons (first l) (insert (rest l) x (sub1 i)))]))
+
 ; Number -> [List-of [List-of Number]]
 ; create identity matrices for a given number
+(check-expect (identityM 1) (list (list 1)))
+(check-expect (identityM 3)
+              (list (list 1 0 0) (list 0 1 0) (list 0 0 1)))
 (define (identityM m)
   (cond
     [(<= m 0) '()]
     [else (local (; list of zeros of length m
-                  (define row (cond
-                                [(= m 1) (make-list m 1)]
-                                [else (make-list m 0)]))
+                  (define row (make-list (sub1 m) 0))
 
                   ; [X] Number -> [List-of [List-of X]]
                   ; create a matrix of lists
@@ -17,16 +26,15 @@
                     (cond
                       [(= 0 m) '()]
                       [else (cons row (make-matrix (sub1 m)))]))
-                  
-                  
-                  ; [List-of [List-of X]] Number -> [List-of [List-of X]]
-                  ; diagonalize
-                  (define (diagonalize matrix n)
+
+
+                  ; [List-of [List-of X]] Number Number -> [List-of [List-of X]]
+                  ; turn a matrix into an identity matrix
+                  (define (make-identity-matrix matrix m)
                     (cond
-                      [(= n 0) '()]
-                      [else (cons
-                             (cons 1 (rest (first matrix)))
-                             (diagonalize (rest matrix) (sub1 n)))]))
+                      [(empty? matrix) '()]
+                      [else (cons (insert (first matrix) 1 (sub1 m))
+                                  (make-identity-matrix (rest matrix) (sub1 m)))]))
                   
-                  ) (diagonalize (make-matrix m) m))]))
-                
+                  ) (reverse (make-identity-matrix (make-matrix m) m)))]))
+
