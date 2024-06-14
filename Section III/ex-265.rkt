923e8a21ec3e7508e88b91e03ec5095f39275fd5
Rubén M. <dev.mantecon@gmail.com>
2024-06-14 12:05:25 +0200

    finish ex-265

---
+++ Section III/ex-265.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-265) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+((local ((define (f x) (+ x 3))
+         (define (g x) (* x 4)))
+   (if (odd? (f (g 1)))
+       f
+       g))
+ 2)
+
+(define (keep-good lon)
+  (filter (lambda (n) (< n 10)) lon))
