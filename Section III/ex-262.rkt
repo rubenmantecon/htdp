ef7bb2ea83b9e951d6ebb904510a0df7e6399a71
Rubén M. <dev.mantecon@gmail.com>
2024-06-14 08:34:42 +0200

    add graphics to ex-262 and finish

--- Section III/ex-262.rkt
+++ Section III/ex-262.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex-262) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex-262) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
 ; [X] [List-of X] X Number -> [List-of X]
 ; insert item at index
 (define (insert l x i)
@@ -27,7 +27,6 @@
                       [(= 0 m) '()]
                       [else (cons row (make-matrix (sub1 m)))]))
 
-
                   ; [List-of [List-of X]] Number Number -> [List-of [List-of X]]
                   ; turn a matrix into an identity matrix
                   (define (make-identity-matrix matrix m)
@@ -37,4 +36,32 @@
                                   (make-identity-matrix (rest matrix) (sub1 m)))]))
                   
                   ) (reverse (make-identity-matrix (make-matrix m) m)))]))
+
+(define (draw-identityM matrix)
+  (local (;; Some graphical constants first
+          (define SIDE 8)
+          (define RED-SQUARE (square SIDE  "solid" "crimson"))
+          (define WHITE-SQUARE (square  SIDE "solid" "white"))
+
+          ; [List-of X] -> Image
+          ; draw a row
+          (define (draw-row row)
+            (cond
+              [(empty? row) '()]
+              [(empty? (rest row)) (if (= (first row) 1) RED-SQUARE WHITE-SQUARE)]
+              [else (if (= (first row) 1)
+                        (beside RED-SQUARE
+                                (draw-row (rest row)))
+                        (beside WHITE-SQUARE
+                                (draw-row (rest row))))]))
+          
+          ; [List-of [List-of X]] -> Image
+          ; draw a matrix
+          (define (draw-matrix matrix)
+            (cond
+              [(empty? matrix) '()]
+              [(empty? (rest matrix)) (draw-row (first matrix))]
+              [else (above (draw-row (first matrix)) (draw-matrix (rest matrix)))]))
+          )
+    (draw-matrix matrix)))
 
