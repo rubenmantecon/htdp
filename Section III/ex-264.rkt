0368a81202b942714490d95063f22866ab6328e0
Rubén M. <dev.mantecon@gmail.com>
2024-06-14 12:05:08 +0200

    finish ex-264

---
+++ Section III/ex-264.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-264) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; Nelon -> Number
+; determines the largest 
+; number on l
+(define (sup l)
+  (cond
+    [(empty? (rest l))
+     (first l)]
+    [else
+     (if (> (first l)
+            (sup (rest l)))
+         (first l)
+         (sup (rest l)))]))
+
+(sup '(1 99 134 4 987 23))
