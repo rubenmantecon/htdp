1885e38df8543fa923458b66137650a214ed7f32
Rubén M. <dev.mantecon@gmail.com>
2024-06-18 15:06:30 +0200

    start ex-274

---
+++ Section III/ex-274.rkt
@@ -0,0 +1,24 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-274) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+; [List-of 1String] -> [List-of [List-of 1String]]
+; produces a list of all the prefixes of a given list of 1String
+(check-expect (prefixes.v2 '()) '())
+(check-expect (prefixes.v2 (list "x")) (list (list "x")))
+(check-expect (prefixes.v2 (list "a" "b" "c"))
+              (list
+               (list "a")
+               (list "a" "b")
+               (list "a" "b" "c")))
+(define (prefixes.v2 l) '())
+
+; [List-of 1String] -> [List-of [List-of 1String]]
+; produces a list of all the sufixes of a given list of 1String
+(check-expect (suffixes.v2 '()) '())
+(check-expect (suffixes.v2 (list "x")) (list (list "x")))
+(check-expect (suffixes.v2 (list "a" "b" "c"))
+              (list
+               (list "a" "b" "c")
+               (list "b" "c")
+               (list "c")))
+(define (suffixes.v2 l)...)
