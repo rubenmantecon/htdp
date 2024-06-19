48585e5aed2f27ab8b2545f9ba11556d7a28bff5
Rubén M. <dev.mantecon@gmail.com>
2024-06-19 10:35:52 +0200

    complete suffixes.v2. So complicated
    This one feels so hard. I don't know how I pulled it off, exactly. I
    just coded constrained by the tests, and went with my intuition. But I
    was not methodical

--- Section III/ex-274.rkt
+++ Section III/ex-274.rkt
@@ -3,14 +3,14 @@
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-274) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
 ; [List-of 1String] -> [List-of [List-of 1String]]
 ; produces a list of all the prefixes of a given list of 1String
-(check-expect (prefixes.v2 '()) '())
-(check-expect (prefixes.v2 (list "x")) (list (list "x")))
-(check-expect (prefixes.v2 (list "a" "b" "c"))
-              (list
-               (list "a")
-               (list "a" "b")
-               (list "a" "b" "c")))
-(define (prefixes.v2 l) '())
+;(check-expect (prefixes.v2 '()) '())
+;(check-expect (prefixes.v2 (list "x")) (list (list "x")))
+;(check-expect (prefixes.v2 (list "a" "b" "c"))
+;              (list
+;               (list "a")
+;               (list "a" "b")
+;               (list "a" "b" "c")))
+;(define (prefixes.v2 l) '() )
 
 ; [List-of 1String] -> [List-of [List-of 1String]]
 ; produces a list of all the sufixes of a given list of 1String
@@ -21,4 +21,14 @@
                (list "a" "b" "c")
                (list "b" "c")
                (list "c")))
-(define (suffixes.v2 l)...)
+(define (suffixes.v2 l)
+  (local (; [List-of 1String] -> [List-of 1String]
+          ; produce a suffix
+          (define (suffix x y)
+            (cond
+              [(empty? x) '()]
+              [(empty? (rest x)) (list x)]
+              [(empty? (rest y)) (list y)]
+              [else (cons y (suffix x (rest y)))])))
+
+    (foldr suffix l (list l))))
