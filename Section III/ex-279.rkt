b44745365077a9d52e66ebf79d373ccad4d4d7cc
Rubén M. <dev.mantecon@gmail.com>
2024-07-09 07:45:54 +0200

    finish ex-279

---
+++ Section III/ex-279.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-279) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp")) #f)))
+; Legal
+(lambda (x y) (x y y))
+
+; Illegal: no parameters
+(lambda () 10)
+
+; Legal
+(lambda (x) x)
+
+; Legal
+(lambda (x y) x)
+
+; Illegal: bad syntax, missing enclosing parenthesis for parameters
+(lambda x 10)
