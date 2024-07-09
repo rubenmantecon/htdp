615f921e0b7432e17462a8d1596de4dc33c1df08
Rubén M. <dev.mantecon@gmail.com>
2024-07-09 07:46:18 +0200

    finish ex-283

---
+++ Section III/ex-283.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-283) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp")) #f)))
+(map (lambda (x) (* 10 x))
+     '(1 2 3))
+ 
+(foldl (lambda (name rst)
+         (string-append name ", " rst))
+       "etc."
+       '("Matthew" "Robby"))
+ 
+(filter (lambda (ir) (<= (IR-price ir) th))
+        (list (make-IR "bear" 10)
+              (make-IR "doll" 33)))
