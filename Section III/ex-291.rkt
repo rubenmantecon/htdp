c3d57a5dd02f948b154cc1a28303ab0e8573aa52
Rubén M. <dev.mantecon@gmail.com>
2024-07-09 07:46:42 +0200

    finish ex-291

---
+++ Section III/ex-291.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-291) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp")) #f)))
+(check-expect (map odd? '(1 2 3)) (map-via-fold odd? '(1 2 3)))
+(define (map-via-fold f l)
+  (foldr (lambda (x acc) (cons (f x) acc)) '() l))
