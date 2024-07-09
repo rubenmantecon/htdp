d27897a42c64570357d9842e521df1c6979c3f07
Rubén M. <dev.mantecon@gmail.com>
2024-07-09 07:48:10 +0200

    can't come up with prefixes()

--- Section III/ex-274.rkt
+++ Section III/ex-274.rkt
@@ -3,14 +3,23 @@
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-274) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
 ; [List-of 1String] -> [List-of [List-of 1String]]
 ; produces a list of all the prefixes of a given list of 1String
-;(check-expect (prefixes.v2 '()) '())
-;(check-expect (prefixes.v2 (list "x")) (list (list "x")))
-;(check-expect (prefixes.v2 (list "a" "b" "c"))
-;              (list
-;               (list "a")
-;               (list "a" "b")
-;               (list "a" "b" "c")))
-;(define (prefixes.v2 l) '() )
+(check-expect (prefixes.v2 '()) '())
+(check-expect (prefixes.v2 (list "x")) (list (list "x")))
+(check-expect (prefixes.v2 (list "a" "b" "c"))
+              (list
+               (list "a" "b" "c")
+               (list "a" "b")
+               (list "a")))
+
+(define (prefixes.v2 l)
+  (local (; [List-of String] -> [List-of String]
+          ; produce a prefix
+          (define (prefix x y)
+          (cond
+            [(empty? x) x]
+            [(empty? y) y]
+            [else (cons (reverse x) (prefix (rest (reverse x)) y))])))
+    (foldr prefix l (list l))))
 
 ; [List-of 1String] -> [List-of [List-of 1String]]
 ; produces a list of all the sufixes of a given list of 1String
@@ -19,16 +28,35 @@
 (check-expect (suffixes.v2 (list "a" "b" "c"))
               (list
                (list "a" "b" "c")
-               (list "b" "c")
+               (list "b" "c") 
                (list "c")))
 (define (suffixes.v2 l)
   (local (; [List-of 1String] -> [List-of 1String]
           ; produce a suffix
-          (define (suffix x y)
+          (define (suffix/inner x y)
             (cond
               [(empty? x) '()]
-              [(empty? (rest x)) (list x)]
-              [(empty? (rest y)) (list y)]
-              [else (cons y (suffix x (rest y)))])))
+              [(empty? y) y]
+              [else (cons y (suffix/inner x (rest y)))])))
+    (foldl suffix/inner l (list l))))
 
-    (foldr suffix l (list l))))
+
+; [List-of String] -> [List-of String]
+; generate the first suffix of x
+(define (suffix acc l)
+            (cond
+              [(empty? acc) '()]
+              [(empty? l) l]
+              [else (cons l (suffix acc (rest l)))]))
+
+; [List-of 1String] -> [List-of [List-of 1String]]
+; produces a list of all the sufixes of a given list of 1String
+(check-expect (suffixes.v3 '()) '())
+(check-expect (suffixes.v3 (list "x")) (list (list "x")))
+(check-expect (suffixes.v3 (list "a" "b" "c"))
+              (list
+               (list "a" "b" "c")
+               (list "b" "c") 
+               (list "c")))
+(define (suffixes.v3 l)
+  (foldl suffix l (list l)))
