8f5769d32ba45a38d248779a99f24881aee59a76
Rubén M. <dev.mantecon@gmail.com>
2024-07-23 13:39:52 +0200

    finish ex-305

---
+++ Intermezzo III/ex-305.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-305) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp")) #f)))
+;; Exercise 305. Use loops to define convert-euro. See exercise 267. 
+
+(define EXCHANGE-RATE 0.92)
+
+; [List-of Number] -> [List-of Number]
+; convert US Dollars to EUR
+(define (convert-euro l)
+  (for/list ([i l]) (* i EXCHANGE-RATE )))
