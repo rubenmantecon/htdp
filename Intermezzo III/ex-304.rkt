2620ccf2063aeec55533de5fb8c6bd766cd62d36
Rubén M. <dev.mantecon@gmail.com>
2024-07-24 07:17:48 +0200

    finish ex-304

---
+++ Intermezzo III/ex-304.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-304) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp")) #f)))
+; [List-of X] -> [List-of X]
+; produce a list of pairs of each item and their respective index
+(define (enumerate l) (for/list ([i (length l)] [j l]) (list (+ i 1) j)))
