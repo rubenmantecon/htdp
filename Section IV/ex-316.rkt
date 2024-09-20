eac8150bec4eca308a71799fb06d5a7b1d5edd11
Rubén M. <dev.mantecon@gmail.com>
2024-09-20 11:06:55 +0200

    finish ex-316

---
+++ Section IV/ex-316.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-316) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp")) #f)))
+; An S-expr is one of: 
+; – Atom
+; – SL
+ 
+; An SL is one of: 
+; – '()
+; – (cons S-expr SL)
+	
+; An Atom is one of: 
+; – Number
+; – String
+; – Symbol
+
+; X -> Boolean
+; is X an atom
+(check-expect (atom? (make-posn 0 0)) #false)
+(check-expect (atom? 'a) #true)
+(define (atom? x) (or (number? x) (string? x) (symbol? x)))
