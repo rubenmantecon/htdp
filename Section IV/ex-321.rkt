6bc8e5c6e68c074df2f52e212983eec096f7ddec
Rubén M. <dev.mantecon@gmail.com>
2024-09-21 12:27:13 +0200

    finish ex-321

---
+++ Section IV/ex-321.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-321) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp")) #f)))
+; An Atom is one of: 
+; – Number
+; – String
+; – Symbol
+
+; An S-expr is either one of:
+; - Atom
+; [List-of S-expr]
