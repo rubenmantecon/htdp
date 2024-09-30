0560351d4057949481129f89280020e4ff97dfa0
Rubén M. <dev.mantecon@gmail.com>
2024-09-30 21:35:14 +0200

    finish ex-330

---
+++ Section IV/ex-330.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-330) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp")) #f)))
+; A Dir.v1 (short for directory) is one of: 
+; – '()
+; – (cons File.v1 Dir.v1)
+; – (cons Dir.v1 Dir.v1)
+ 
+; A File.v1 is a String.
+
+(define DT '(("part 1" "part 2" "part 3")
+             "read!"
+             (("hang" "draw")
+              ("read!"))))
