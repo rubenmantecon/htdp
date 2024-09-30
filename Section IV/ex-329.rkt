a2ef5127acec63a050cf56452d490771cdc33077
Rubén M. <dev.mantecon@gmail.com>
2024-09-30 21:35:08 +0200

    finish ex-329

---
+++ Section IV/ex-329.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-329) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "abstraction.rkt" "teachpack" "2htdp")) #f)))
+;; read! occurs two (2) times
+
+;; There are two paths:
+;; - TS (DIR) -> read! (10)
+;; - TS (DIR) -> Libs (DIR) -> Docs (DIR) -> read! (19)
+
+;; The total size of all files in the tree is 207
+
+;; The total size, of the directory tree if each node has size 1 is 212
+
+;; There are four (4) levels, starting at the root
